43c84f5aaa26d57f5f18ba3df5cdd08da548b69e
AkaneSakuramori <utkarshffuff@gmail.com>
2025-08-27 13:23:07 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------------------
+# Copyright (c) 2021-2025
+# Developers: TheAlphaBotz
+# Telegram: @TheAlphaBotz
+# License: All Rights Reserved
+# ---------------------------------------------------------
+
+API_ID = 25482744
+API_HASH = "e032d6e5c05a5d0bfe691480541d64f4"
+BOT_TOKEN = "8026632681:AAGb5yt4iVU8jdznzCuw1Ca99ban0dR6zW4"
+
+DOWNLOAD_DIR = "downloads"
