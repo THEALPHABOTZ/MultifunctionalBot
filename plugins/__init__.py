ce3fcf4748acf2ebf23c283a1429495645d76550
AkaneSakuramori <utkarshffuff@gmail.com>
2025-08-27 13:22:04 +0530

    Create __init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,6 @@
+# ---------------------------------------------------------
+# Copyright (c) 2021-2025
+# Developers: TheAlphaBotz
+# Telegram: @TheAlphaBotz
+# License: All Rights Reserved
+# ---------------------------------------------------------
